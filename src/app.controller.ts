cb173b5f3d269295bdde552c65c92cad1e0675df
Ray B <vladislav.foxivich@gmail.com>
2025-08-30 20:23:24 -0500

    added some validators

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateDto } from './dto/create.dto';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('get/:id')
+  getId(@Param('id', ParseIntPipe) id: number): number {
+    if (id < 1) {
+      throw new BadRequestException('Error');
+    }
+    return id;
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Post('create')
+  create(@Body() dto: CreateDto) {
+    console.log('post');
+    return dto;
   }
 }
